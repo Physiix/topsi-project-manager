6cc40186e305ae3de84802544b22e43936e44159
Physix <yanis.m.1992@gmail.com>
2018-01-22 15:49:53 -0500

    Git classa added and github dependency added.

---
+++ src/core/git.js
@@ -0,0 +1,19 @@
+class GitUserInfo {
+	constructor() {
+		this.username = ''
+		this.token = ''
+		this.gist_token = ''
+	}
+}
+
+class Git {
+	/**
+	 * Authenticate the user to github.
+	 * @param {*GitUserInfo} GitInfo Information about the user.
+	 */
+	Authenticate(info) {
+
+	}
+
+
+}
