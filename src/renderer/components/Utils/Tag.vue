4c6ed85767d1c3b633988ebaa0dca32112cd55ae
Physix <yanis.m.1992@gmail.com>
2018-08-19 09:18:18 -0400

    Added tag component.

---
+++ src/renderer/components/Utils/Tag.vue
@@ -0,0 +1,36 @@
+<template>
+	<v-chip ref="tag" class="tag" :color="color?color:''" small :text-color="textColor?textColor:''">
+		<strong>
+			<slot></slot>
+		</strong>
+	</v-chip>
+</template>
+<script>
+
+export default {
+	name: 'Tag',
+	props: {
+		color: String,
+		textColor: String
+	},
+	data() {
+		return {
+
+		}
+	},
+	mounted() {
+		const element = this.$refs.tag.$el;
+		element.style.zIndex = 0;
+	}
+}
+</script>
+
+<style >
+.tag{
+	border-radius:8%;
+	height: 20px;
+}
+.v-chip__content{
+	padding: 5px!important;
+}
+</style>
