eee3815fc154572cbe82622a25c0a318fe8fbecf
Physix <yanis.m.1992@gmail.com>
2018-08-24 14:58:13 -0400

    Updated colors and firstuse component.

--- src/renderer/components/Temp/FirstUse.vue
+++ src/renderer/components/Temp/FirstUse.vue
@@ -7,35 +7,17 @@
 				</v-card-title>
 
 				<v-card-text class="text-xs-center">
-					<strong>Press CTRL+D at any time to display the help page.</strong>
-					Since this is the first time you're using this application, please setup the next default values to be used by the application. <br/> <br/> All the settings can be
+					<strong>Press CTRL+D at any time to display the help page.</strong> <br /> Since this is the first time you're using this application, please setup the next default values to be used by the application. <br/> <br/> All the settings can be
 					<strong>modified </strong> at any time.
 				</v-card-text>
 
 				<v-divider class="py-4"></v-divider>
 				<v-card-title>
-					Setup the default folder to store your projects.
-					<v-layout row>
-						<v-flex xs8>
-							<v-layout row>
-								<v-text-field class="pt-2 mt-1" value="path/to/default/folder" v-model="defaultFolder"></v-text-field>
-								<v-icon v-if="folderExists" color="success" class="mt-3">
-									check
-								</v-icon>
-								<v-icon v-else color="error" class="mt-3">
-									close
-								</v-icon>
-							</v-layout>
-						</v-flex>
-						<v-flex xs4>
-							<v-btn @click="OpenDialog" class="ml-5 justify-right text-xs-right" flat>
-								Browse
-							</v-btn>
-						</v-flex>
-					</v-layout>
+					<p class="ma-0 ml-4">Setup the default folder to store your projects.</p>
+					<FolderInput v-model="defaultFolder"></FolderInput>
 				</v-card-title>
 				<v-card-title class="pa-0">
-					<v-card-text>Setup the default application color</v-card-text>
+					<v-card-text class="ma-0 ml-4">Setup the default application color</v-card-text>
 					<ColorPicker width="600" padding="10" v-on:color-selected="SelectColor"></ColorPicker>
 				</v-card-title>
 				<v-divider></v-divider>
@@ -51,10 +33,7 @@
 </template>
 
 <script>
-const fs = require('fs');
 const os = require('os');
-const path = require('path');
-const electron = require('electron');
 
 export default {
 	name: 'FirstUse',
@@ -97,8 +76,8 @@ export default {
 		}
 	},
 
-	mounted() {
-		this.defaultFolder = path.join(os.homedir(), 'Projects');
+	created() {
+		this.defaultFolder = os.homedir();
 	}
 }
 </script>
--- src/renderer/components/Utils/FolderInput.vue
+++ src/renderer/components/Utils/FolderInput.vue
@@ -1,21 +1,19 @@
 <template>
-	<div>
-		<v-toolbar class="mx-2 pa-2 px-4 transparent elevation-0">
-			<v-text-field class="pt-2 mt-1" light v-model="folder" solo></v-text-field>
-			<div>
-				<v-icon v-if="folderExists" color="success">
-					check
-				</v-icon>
-				<v-icon v-else color="error">
-					close
-				</v-icon>
-			</div>
-			<v-btn @click="OpenDialog" class="elevation-5 justify-right text-xs-right" color="primary" style="border-radius:0;">
-				Browse
-			</v-btn>
+	<v-toolbar class="mx-2 pa-2 px-4 transparent elevation-0">
+		<v-text-field label="Folder" class="pt-2 mt-1" light v-model="folder" solo></v-text-field>
+		<div>
+			<v-icon v-if="folderExists" color="success">
+				check
+			</v-icon>
+			<v-icon v-else color="error">
+				close
+			</v-icon>
+		</div>
+		<v-btn @click="OpenDialog" class="elevation-5 justify-right text-xs-right" color="primary" style="border-radius:0;">
+			Browse
+		</v-btn>
 
-		</v-toolbar>
-	</div>
+	</v-toolbar>
 </template>
 <script>
 const fs = require('fs');
