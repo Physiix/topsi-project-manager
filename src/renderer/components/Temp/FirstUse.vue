1843cfc28c8f09a0da00ecb760c8c8bb7ac1d0c4
Physiix <yanis.m.1992@gmail.com>
2018-08-25 02:43:15 -0400

    Fixed issue in FirstUse component.

--- src/renderer/components/Temp/FirstUse.vue
+++ src/renderer/components/Temp/FirstUse.vue
@@ -2,28 +2,26 @@
 	<div class="text-xs-center">
 		<v-dialog v-model="dialog" width="600" persistent>
 			<v-card>
-				<v-card-title class="headline justify-center" dark primary-title>
-					{{this.lang.Get('firstUseWelcome')}}
+				<v-card-title class="headline justify-center" dark primary-title v-html="this.$lang.Get('firstUseWelcome')">
 				</v-card-title>
 
-				<v-card-text class="text-xs-center">
-					{{this.lang.Get('firstUseContent')}}
+				<v-card-text class="text-xs-center" v-html="this.$lang.Get('firstUseContent')">
 				</v-card-text>
 
 				<v-divider class="py-4"></v-divider>
 				<v-card-title>
-					<p class="ma-0 ml-4">{{this.lang.Get('firstUseSetupFolder')}}</p>
+					<p class="ma-0 ml-4">{{this.$lang.Get('firstUseSetupFolder')}}</p>
 					<FolderInput v-model="defaultFolder"></FolderInput>
 				</v-card-title>
 				<v-card-title class="pa-0">
-					<v-card-text class="ma-0 ml-4">{{this.lang.Get('firstUseSetupColor')}}</v-card-text>
+					<v-card-text class="ma-0 ml-4">{{this.$lang.Get('firstUseSetupColor')}}</v-card-text>
 					<ColorPicker width="600" padding="10" v-on:color-selected="SelectColor"></ColorPicker>
 				</v-card-title>
 				<v-divider></v-divider>
 				<v-card-actions>
 					<v-spacer></v-spacer>
 					<v-btn color="primary" flat @click="Done">
-						{{this.lang.Get('done')}}
+						{{this.$lang.Get('done')}}
 					</v-btn>
 				</v-card-actions>
 			</v-card>
