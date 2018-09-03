92ce2b1b08193fdecbf0829779e44ba94a571ee0
Physiix <yanis.m.1992@gmail.com>
2018-09-03 03:29:00 -0400

    Using a queue for notifications and dynamic height supported.

--- src/core/Notifications.js
+++ src/core/Notifications.js
@@ -1,12 +1,29 @@
-const DEFAULT_TIME = 3000;
+const DEFAULT_TIME = 5000;
 export class NotificationEntry {
-	constructor(title, description, timeout) {
+	constructor(title, description, timeout, color) {
 		this.title = title;
 		this.description = description;
 		this.timeout = timeout;
+		this.color = color;
 	}
 }
 class Notifications {
+	constructor() {
+		// Contains the notifications to display.
+		this.queue = [];
+
+		// Whether it's ready do display.
+		this.ready = true;
+
+		// Loop and display the notifications in the queues
+		const queuId = setInterval(() => {
+			if (this.ready && this.queue.length > 0) {
+				this.ready = false;
+				this.Notify(this.queue.pop());
+			}
+		}, 150);
+	}
+
 	/**
 	 * 	Display an info notification in the screen.
 	 * @param {String} title Title of the notification to display.
@@ -14,7 +31,7 @@ class Notifications {
 	 * @param {Number} timout Time before destroying the notification.
 	 */
 	Info(title, description = '', timeout = DEFAULT_TIME) {
-		this.Notify(new NotificationEntry(title, description, timeout), 'primary white--text');
+		this.queue.push(new NotificationEntry(title, description, timeout, 'primary white--text'));
 	}
 
 	/**
@@ -24,7 +41,7 @@ class Notifications {
 	 * @param {Number} timout Time before destroying the notification.
 	 */
 	Error(title, description = '', timeout = DEFAULT_TIME) {
-		this.Notify(new NotificationEntry(title, description, timeout), 'error white--text');
+		this.queue.push(new NotificationEntry(title, description, timeout, 'error white--text'));
 		throw new Error(`${title} ${description}`);
 	}
 
@@ -36,7 +53,7 @@ class Notifications {
 	 * @param {Number} timout Time before destroying the notification.
 	 */
 	Warning(title, description = '', timeout = DEFAULT_TIME) {
-		this.Notify(new NotificationEntry(title, description, timeout), 'warning white--text');
+		this.queue.push(new NotificationEntry(title, description, timeout, 'warning white--text'));
 	}
 
 
@@ -47,18 +64,39 @@ class Notifications {
 	 * @param {Number} timout Time before destroying the notification.
 	 */
 	Success(title, description = '', timeout = DEFAULT_TIME) {
-		this.Notify(new NotificationEntry(title, description, timeout), 'success white--text');
+		this.queue.push(new NotificationEntry(title, description, timeout, 'success white--text'));
 	}
 
 
 	/**
 	 * Setup the notification system. 
 	 * @param {NotificationEntry} entry Contains the info about the notification.
-	 * @param {String} color Color of the div displaying the notification.
 	 */
-	Notify(entry, color) {
+	Notify(entry) {
 		const nElement = document.createElement('div');
+		document.querySelector('body').appendChild(nElement);
+
+		// Basic customization 
+		const color = entry.color;
 		const left = window.innerWidth - 260;
+		nElement.className = color + ' notification-card';
+		nElement.style.position = 'fixed';
+		nElement.style.width = '250px';
+		nElement.style.left = left + 'px';
+
+		// Populating the notification its info.
+		const eTitle = document.createElement('div');
+		eTitle.className = 'v-card__title title pa-2';
+		eTitle.innerHTML = entry.title;
+		const eDesc = document.createElement('div');
+		eDesc.className = 'v-card__text subheading px-2';
+		eDesc.innerHTML = (entry.description) ? entry.description : '';
+		nElement.appendChild(eTitle);
+		nElement.appendChild(eDesc);
+
+		// Calculating the dimnesions
+		const height = nElement.clientHeight;
+		const totalHeight = height + 10;
 
 		// When a new notification is added, make sure to move all the other.
 		const nots = document.getElementsByClassName('notification-card');
@@ -70,32 +108,25 @@ class Notifications {
 			const moveId = setInterval(() => {
 				moveTop += 6;
 				item.style.top = rect.top - moveTop + 'px';
-				if (moveTop >= 110) {
-					item.style.top = rect.top - 110 + 'px';
+				if (moveTop >= totalHeight) {
+					item.style.top = rect.top - totalHeight + 'px';
 					clearInterval(moveId);
 				}
 			})
 		}
-
-		// Basic customization 
-		nElement.className = color + ' notification-card';
-		nElement.style.position = 'fixed';
-		nElement.style.width = '250px';
-		nElement.style.height = '100px';
-		nElement.style.left = left + 'px';
 
 		// Entering animation.
 		let enterTop = 0;
 		const enterId = setInterval(() => {
 			enterTop += 6;
 			nElement.style.top = window.innerHeight - enterTop + 'px';
-			if (enterTop >= 110) {
-				nElement.style.top = window.innerHeight - 110 + 'px';
+			if (enterTop >= totalHeight) {
+				nElement.style.top = window.innerHeight - totalHeight + 'px';
+				this.ready = true;
 				clearInterval(enterId);
 			}
 		})
-		nElement.style.top = window.innerHeight - 110 + 'px';
-		document.querySelector('body').appendChild(nElement);
+		nElement.style.top = window.innerHeight - totalHeight + 'px';
 
 		// Exiting animation.
 		setTimeout(() => {
@@ -110,17 +141,6 @@ class Notifications {
 			})
 
 		}, entry.timeout ? entry.timeout : DEFAULT_TIME);
-
-		// Populating the notification its info.
-		const eTitle = document.createElement('div');
-		eTitle.className = 'v-card__title title pa-2';
-		eTitle.innerHTML = entry.title;
-
-		const eDesc = document.createElement('div');
-		eDesc.className = 'v-card__text subheading px-2';
-		eDesc.innerHTML = (entry.description) ? entry.description : '';
-		nElement.appendChild(eTitle);
-		nElement.appendChild(eDesc);
 	}
 }
 
