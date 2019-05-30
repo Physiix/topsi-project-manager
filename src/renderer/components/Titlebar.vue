a397929beba2d4bc3fd4ed8cfda490de49de236d
Physiix <yanis.m.1992@gmail.com>
2019-05-30 15:06:50 -0400

    Updates formatting.

--- src/renderer/components/Titlebar.vue
+++ src/renderer/components/Titlebar.vue
@@ -6,53 +6,21 @@
             {{this.$lang.Get('projectName')}} {{projectName}}
             <v-spacer></v-spacer>
             <v-btn flat class="titlebar-btn mx-0" @click="minimize">
-                <svg
-                    width="9"
-                    height="9"
-                    viewBox="0 0 11 11"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
+                <svg width="9" height="9" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11 4.399V5.5H0V4.399h11z" fill="#fff"></path>
                 </svg>
             </v-btn>
             <v-btn flat class="titlebar-btn mx-0" @click="maximise">
-                <svg
-                    v-if="!maximized"
-                    width="9"
-                    height="9"
-                    viewBox="0 0 11 11"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
+                <svg v-if="!maximized" width="9" height="9" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11 0v11H0V0h11zM9.899 1.101H1.1V9.9H9.9V1.1z" fill="#fff"></path>
                 </svg>
-                <svg
-                    v-else
-                    width="9"
-                    height="9"
-                    viewBox="0 0 11 11"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M11 8.798H8.798V11H0V2.202h2.202V0H11v8.798zm-3.298-5.5h-6.6v6.6h6.6v-6.6zM9.9 1.1H3.298v1.101h5.5v5.5h1.1v-6.6z"
-                        fill="#fff"
-                    ></path>
+                <svg v-else width="9" height="9" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M11 8.798H8.798V11H0V2.202h2.202V0H11v8.798zm-3.298-5.5h-6.6v6.6h6.6v-6.6zM9.9 1.1H3.298v1.101h5.5v5.5h1.1v-6.6z" fill="#fff"></path>
                 </svg>
             </v-btn>
             <v-btn flat class="titlebar-btn mx-0 close-btn" @click="close">
-                <svg
-                    width="9"
-                    height="9"
-                    viewBox="0 0 11 11"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        d="M6.279 5.5L11 10.221l-.779.779L5.5 6.279.779 11 0 10.221 4.721 5.5 0 .779.779 0 5.5 4.721 10.221 0 11 .779 6.279 5.5z"
-                        fill="#fff"
-                    ></path>
+                <svg width="9" height="9" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M6.279 5.5L11 10.221l-.779.779L5.5 6.279.779 11 0 10.221 4.721 5.5 0 .779.779 0 5.5 4.721 10.221 0 11 .779 6.279 5.5z" fill="#fff"></path>
                 </svg>
             </v-btn>
         </v-toolbar>
--- src/renderer/components/drawer/GeneralSettings.vue
+++ src/renderer/components/drawer/GeneralSettings.vue
@@ -1,20 +1,10 @@
 <template>
     <v-card>
         <v-container>
-            <v-checkbox
-                class="px-1"
-                style="padding-left:13px;"
-                :label="$lang.Get('darkMode')"
-                v-model="darkMode"
-            ></v-checkbox>
-            <v-checkbox
-                class="px-1"
-                style="padding-left:13px;"
-                :label="$lang.Get('darkenToolbar')"
-                v-model="darkenToolbar"
-            ></v-checkbox>
+            <v-checkbox class="px-1" style="padding-left:13px;" :label="$lang.Get('darkMode')" v-model="darkMode"></v-checkbox>
+            <v-checkbox class="px-1" style="padding-left:13px;" :label="$lang.Get('darkenToolbar')" v-model="darkenToolbar"></v-checkbox>
             <ColorPicker width="450" padding="7" class v-on:color-selected="SelectColor"></ColorPicker>
-            <LanguageSelection class="px-2"/>
+            <LanguageSelection class="px-2" />
         </v-container>
     </v-card>
 </template>
