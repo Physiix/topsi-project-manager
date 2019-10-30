e3f1c6ae67b3132869f4e729998557fb949deb30
Yanis <yanis.m.1992@gmail.com>
2019-10-30 19:56:36 -0400

    Adds Animation to Note

--- src/renderer/components/notes/Note.vue
+++ src/renderer/components/notes/Note.vue
@@ -1,81 +1,97 @@
 <template>
-	<v-card :id="'note-'+note.id" class="note ma-2" :class="note.color" @dblclick="Open" :dark="dark">
-		<v-card-title class="py-2 pt-3 pr-0">
-			<v-layout row>
-				<div class="blue--text pr-1">
-					<strong>#{{note.id}}</strong>
-				</div>
-				<h4>{{note.title}}</h4>
-				<v-spacer></v-spacer>
-				<v-btn small icon class="ma-0" @click="Edit">
-					<v-icon style="font-size:16px;">
-						edit
-					</v-icon>
-				</v-btn>
-			</v-layout>
-		</v-card-title>
-		<v-divider></v-divider>
-		<NoteTags class="px-2" :note-color="note.color" :tags="note.tags" />
-		<v-card-text v-html="note.description"></v-card-text>
-	</v-card>
+  <v-card :id="'note-'+note.id" class="note ma-2" :class="note.color" @dblclick="Open" :dark="dark">
+    <v-card-title class="py-2 pt-3 pr-0">
+      <v-layout row>
+        <div class="blue--text pr-1">
+          <strong>#{{note.id}}</strong>
+        </div>
+        <h4>{{note.title}}</h4>
+        <v-spacer></v-spacer>
+        <v-btn small icon class="ma-0" @click="Edit">
+          <v-icon style="font-size:16px;">edit</v-icon>
+        </v-btn>
+      </v-layout>
+    </v-card-title>
+    <v-divider></v-divider>
+    <NoteTags class="px-2" :note-color="note.color" :tags="note.tags" />
+    <v-card-text v-html="note.description"></v-card-text>
+  </v-card>
 </template>
 <script>
-import NoteTags from './NoteTags.vue'
+import NoteTags from "./NoteTags.vue";
 
 export default {
-	name: 'Note',
-	components: {
-		NoteTags
-	},
-	props: {
-		note: Object
-	},
-	methods: {
-		Edit() {
-			this.$store.dispatch('EditNote', this.note);
-		},
+  name: "Note",
+  components: {
+    NoteTags
+  },
+  props: {
+    note: Object
+  },
+  methods: {
+    Edit() {
+      this.$store.dispatch("EditNote", this.note);
+    },
 
-		Open() {
-			this.$store.dispatch('VisualizeNote', this.note);
-		}
-	},
-	computed: {
-		dark() {
-			const darkMode = this.$store.state.AppStore.darkMode && this.note.color == '';
-			return (darkMode) ? true : (this.note.color.includes('white--text')) ? true : false;
-		}
-	}
-}
+    Open() {
+      this.$store.dispatch("VisualizeNote", this.note);
+    }
+  },
+  computed: {
+    dark() {
+      const darkMode =
+        this.$store.state.AppStore.darkMode && this.note.color == "";
+      return darkMode
+        ? true
+        : this.note.color.includes("white--text")
+        ? true
+        : false;
+    }
+  }
+};
 </script>
 
 <style>
-.color_btn{
-	border-radius: 0;
-	min-width: 10px !important;
-	max-height: 10px !important;
-	margin: 0;
+.color_btn {
+  border-radius: 0;
+  min-width: 10px !important;
+  max-height: 10px !important;
+  margin: 0;
 }
 
-.note{
-	cursor: pointer;
-	transition: 0.1s;
-}
-.note:hover{
-	opacity: 0.8;
+.note {
+  cursor: pointer;
+  transition: 0.1s;
+
+  animation: show-note 200ms ease-in-out;
 }
 
-.note-btn{
-	min-width:20px!important;
-	max-width:20px!important;
-	height:20px;
+@keyframes show-note {
+  0% {
+    transform: translateY(30%);
+  }
+
+  100% {
+    transform: translateY(0);
+  }
 }
 
-.v-card__text img{
-	width: 100%;
+.note:hover {
+  opacity: 0.8;
 }
 
-.tag{
-	border-radius:5%!important;
-	z-index: 0;
+.note-btn {
+  min-width: 20px !important;
+  max-width: 20px !important;
+  height: 20px;
+}
+
+.v-card__text img {
+  width: 100%;
+}
+
+.tag {
+  border-radius: 5% !important;
+  z-index: 0;
 }
 </style>
